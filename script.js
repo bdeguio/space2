a2ed91c1838e811c7430e360c9e7bdba040beea6
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-03-03 18:27:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,11 +14,9 @@ function toggleSidebar() {
 function loadPdf(pdfFile) {
     const pdfViewer = document.getElementById("pdf-viewer");
     const mainHeading = document.getElementById("main-heading");
-    const closeBtn = document.getElementById("close-btn");
 
     pdfViewer.style.display = "block";
     mainHeading.style.display = "none"; // Hide heading when a PDF is opened
-    closeBtn.style.display = "block"; // Show Close button
 
     pdfViewer.src = pdfFile; // Load the selected PDF
 }
@@ -26,10 +24,8 @@ function loadPdf(pdfFile) {
 function closePdfViewer() {
     const pdfViewer = document.getElementById("pdf-viewer");
     const mainHeading = document.getElementById("main-heading");
-    const closeBtn = document.getElementById("close-btn");
 
     pdfViewer.style.display = "none";
-    closeBtn.style.display = "none"; // Hide Close button
     mainHeading.style.display = "block"; // Show heading again when closing PDF
 
     pdfViewer.src = ""; // Reset the iframe source
