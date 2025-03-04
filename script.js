c159b81af40e7f3ee83a4d012bf9ec6337c6044a
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-03-03 16:17:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const text = document.getElementById("sidebar-text");
     const pdfList = document.getElementById("pdf-list");
+    const closeBtn = document.getElementById("close-btn");
 
     if (sidebar.classList.contains("expanded")) {
         sidebar.classList.remove("expanded");
         text.style.display = "none";
         pdfList.style.display = "none";
+        closeBtn.style.display = "none";
     } else {
         sidebar.classList.add("expanded");
         text.style.display = "block";
         pdfList.style.display = "block";
+        closeBtn.style.display = "block";
     }
 }
 
 function loadPdf(pdfFile) {
     const pdfViewer = document.getElementById("pdf-viewer");
-    const closeBtn = document.getElementById("close-btn");
     const mainHeading = document.getElementById("main-heading");
 
     pdfViewer.style.display = "block";
-    closeBtn.style.display = "block";
     mainHeading.style.display = "none"; // Hide heading when viewing PDF
 
     pdfViewer.src = pdfFile; // Assumes the PDFs are in the same directory as index.html
